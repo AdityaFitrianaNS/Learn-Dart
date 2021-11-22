2a946958ed5941db7155344097f02b7e6805aa2b
Aditya Fitriana Nursoleh <89055857+AdityaFitrianaNS@users.noreply.github.com>
2021-11-22 17:17:18 +0700

    docs: progress belajar dart

---
+++ 01_hello_word/hello_world.dart
@@ -0,0 +1,5 @@
+// Diawali dengan void main sebelum memulai.
+void main() {
+  print('Hello Aditya'); // Perintah print menggunakan tanda kutip 1 ''
+  print("Hello Aditya FNS"); // Dan bisa juga menggunakan tanda kutip 2 ""
+}
---
+++ 02_variable/variable_string.dart
@@ -0,0 +1,18 @@
+void main() {
+  //variable string
+  String name;
+  name = 'Aditya Fitriana Nursoleh';
+
+  print(name);
+  print(name);
+  print(name);
+  print(name);
+
+  //Deklarasi variable langsung
+  String nama = 'Aditya Fitriana Nursoleh';
+
+  print(nama);
+  print(nama);
+  print(nama);
+  print(nama);
+}
