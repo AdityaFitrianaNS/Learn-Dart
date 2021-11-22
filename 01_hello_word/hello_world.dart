15566533cc00b1718d33c275a2eb2e972e16e46b
Aditya Fitriana Nursoleh <aditya.fns04@gmail.com>
2021-11-22 17:19:44 +0700

    docs: progress belajar dart

--- 01_hello_word/hello_world.dart
+++
@@ -1,5 +0,0 @@
-// Diawali dengan void main sebelum memulai.
-void main() {
-  print('Hello Aditya'); // Perintah print menggunakan tanda kutip 1 ''
-  print("Hello Aditya FNS"); // Dan bisa juga menggunakan tanda kutip 2 ""
-}
---
+++ basic/02_variable/final.dart
@@ -0,0 +1,12 @@
+void main() {
+  var firstName = 'Aditya'; // Bisa dideklarasikan ulang.
+  final lastName = 'FNS'; // Tidak bisa dideklarasikan ulang.
+
+  print(firstName);
+  print(lastName);
+
+  // Mengubah isi/value.
+
+  firstName = 'Aditya FNS';
+  print(firstName);
+}
