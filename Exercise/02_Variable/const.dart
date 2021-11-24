e9416413b9a674f5651dcc959ab70ce875cb4c8b
Aditya Fitriana NS <89055857+AdityaFitrianaNS@users.noreply.github.com>
2021-11-24 10:23:09 +0700

    Latihan const

---
+++ Exercise/02_Variable/const.dart
@@ -0,0 +1,23 @@
+void main() {
+  var name = 'Belajar Dart Dasar';
+  var number = 'Kelas 0';
+  var kelas = 'がんばって！';
+  final level = [1, 2, 3];
+  const hp = '08123456789';
+
+  print(name);
+  print(kelas);
+
+  name = 'Aditya Fitriana NS';
+  number = 'Kelas 1';
+  kelas = 'Beginner';
+  level[0] = 01;
+  level[1] = 02;
+  level[2] = 03;
+
+  print(name);
+  print(number);
+  print(kelas);
+  print(level);
+  print(hp);
+}
