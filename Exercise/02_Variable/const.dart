5d55cdd17e5454ef1bd095f33b77c864aee0e528
Aditya Fitriana Nursoleh <aditya.fns04@gmail.com>
2021-11-26 10:37:34 +0700

    update docs

--- Exercise/02_Variable/const.dart
+++ Exercise/02_Variable/const.dart
@@ -1,23 +1,46 @@
 void main() {
+  //Latihan 1
   var name = 'Belajar Dart Dasar';
   var number = 'Kelas 0';
-  var kelas = 'がんばって！';
+  var quotes = 'がんばって！';
   final level = [1, 2, 3];
   const hp = '08123456789';
 
   print(name);
-  print(kelas);
+  print(quotes);
 
   name = 'Aditya Fitriana NS';
   number = 'Kelas 1';
-  kelas = 'Beginner';
+  quotes = 'Semangat!';
   level[0] = 01;
   level[1] = 02;
   level[2] = 03;
 
   print(name);
   print(number);
-  print(kelas);
+  print(quotes);
   print(level);
   print(hp);
+
+  //Latihan 2
+  var nama = 'Aditya FNS';
+  var kelas = 'R51';
+  final nim = [4, 5, 6];
+  const note = 'がんばります!';
+
+  print(nama);
+  print(kelas);
+  print(nim);
+  print(note);
+
+  nama = 'Aditya Fitriana NS';
+  kelas = 'R5';
+  nim[0] = 1;
+  nim[1] = 2;
+  nim[2] = 3;
+
+  print(nama);
+  print(kelas);
+  print(nim);
+  print(note);
 }
